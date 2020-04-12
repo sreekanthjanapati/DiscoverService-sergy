7b3ce179865d438e93319108a75eb2fa25a27a17
sreekanthjanapati <sreekanth@test.com>
2020-04-12 13:17:24 +0530

    first time comitting master branch

---
+++ src/main/java/com/microservices/DiscoverServiceSergyApplication.java
@@ -0,0 +1,13 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverServiceSergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverServiceSergyApplication.class, args);
+	}
+
+}
